0aa3510dc94bcddcbfd917fe820bd7639e6f42dc
Luqman Azad <62448969+LuqmanAzad@users.noreply.github.com>
2022-12-26 01:24:09 +0300

    Update question.php

--- Project_1/php/question.php
+++ Project_1/php/question.php
@@ -1,5 +1,21 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT");
+header("Access-Control-Allow-Headers: X-Custom-Header");
+header("Access-Control-Allow-Credentials: true");
+require_once 'php/connection.php';
 
-require_once 'connection.php';
+$sql = "SELECT * FROM question LIMIT 1";
+$result = mysqli_query($conn, $sql);
 
+if (mysqli_num_rows($result) > 0) {
+  $row = mysqli_fetch_assoc($result);
+  // Return the question as a JSON object
+  echo json_encode($row);
+} else {
+  echo "No questions found";
+}
 
+mysqli_close($conn);
+
+?>
