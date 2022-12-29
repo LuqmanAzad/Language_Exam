e91082cf735c1379b8276b90bb75eb851813ed58
Luqman Azad <62448969+LuqmanAzad@users.noreply.github.com>
2022-12-29 04:51:40 +0300

    Update question.php

--- Project_1/php/question.php
+++ Project_1/php/question.php
@@ -3,17 +3,22 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT");
 header("Access-Control-Allow-Headers: X-Custom-Header");
 header("Access-Control-Allow-Credentials: true");
-require_once 'connection.php';
+require_once 'php/connection.php';
 
-$sql = "SELECT * FROM question LIMIT 1";
+$offset = 0;
+if (isset($_GET['offset'])) {
+  $offset = intval($_GET['offset']);
+}
+
+$sql = "SELECT * FROM question LIMIT 1 OFFSET $offset";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-  $row = mysqli_fetch_assoc($result);
-  // Return the question as a JSON object
+    $row = mysqli_fetch_assoc($result);
+    // Return the question as a JSON object
   echo json_encode($row);
 } else {
-  echo "No questions found";
+    echo "No questions found";
 }
 
 mysqli_close($conn);
